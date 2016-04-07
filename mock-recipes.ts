60c8e166ebc965d4f2bddfe7abf3b873d62600f6
mcmichael <michael.mena@gmail.com>
2016-04-07 16:26:15 -0400

    Test angular 2 recipes with services
    Included services. :)

---
+++ mock-recipes.ts
@@ -0,0 +1,15 @@
+import {Recipe} from './recipe'
+
+export var Recipes: Recipe[] = [
+	{ "id": 1, "name": "Arroz chaufa" },
+	{ "id": 2, "name": "Arroz con pollo" },
+	{ "id": 3, "name": "Pollo al horno" },
+	{ "id": 4, "name": "Tallarines verdes" },
+	{ "id": 5, "name": "Aji de gallina" },
+	{ "id": 6, "name": "Tallarines rojos" },
+	{ "id": 7, "name": "Seco de pollo" },
+	{ "id": 8, "name": "Tallarin saltado con carne" },
+	{ "id": 9, "name": "Estofado de pollo" },
+	{ "id": 10, "name": "Sopa de pollo" },
+	{ "id": 11, "name": "Papa rellena" }
+];
